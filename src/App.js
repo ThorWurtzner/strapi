1b974999693c83873958ccff2af31686dc05f34d
Thor Würtzner <6273158@rts-365.dk>
2021-05-31 14:48:51 +0200

    ready for components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import './App.scss';
 import axios from "axios";
 import { useEffect, useState } from "react";
 import parse from "html-react-parser";
+import { Router } from "@reach/router";
+import Home from "./pages/Home/Home";
+import Gallery from "./pages/Gallery/Gallery";
+import Posts from "./pages/Posts/Posts";
 
 function App() {
 
   var [posts, setPosts] = useState([]);
-  var [date, setDate] = useState('');
   var [heading, setHeading] = useState('');
 
   useEffect(() => {
     axios.get('http://localhost:1337/posts')
       .then(response => {
+        // // ISO format
+        // var date = post.published_at;
+        // // Instance new class so it's an object and pass ISO string.
+        // var d = new Date(date);
+        // // Use method to convert to UTC
+        // var d = d.toUTCString();
+        // // Remove unwanted hour and second from string
+        // d = d.slice(0, 16)
+
         setPosts(response.data);
       });
 
@@ -19,34 +31,15 @@ function App() {
       .then(response => {
         setHeading(response.data.Heading);
       });
-  }, [setDate, setPosts, setHeading])
+  }, [setPosts, setHeading])
 
   return (
-    <div>
-
-      { posts?.map(post => {
-
-        // ISO format
-        var date = post.published_at;
-        // Instance new class so it's an object and pass ISO string.
-        var d = new Date(date);
-        // Use method to convert to UTC
-        var d = d.toUTCString();
-        // Remove unwanted hour and second from string
-        d = d.slice(0, 16)
-        
-        return(
-          <div key={post.Title}>
-            <p>{d}</p>
-            <p>{post.Title}</p>
-            <p>{parse(post.Text)}</p>
-          </div>
-        )
-      })}
-
-      <p>{heading}</p>
-    </div>
-  );
+    <Router>
+      <Home path="/" />
+      <Posts path="/posts" />
+      <Gallery path="gallery" />
+    </Router>
+  )
 }
 
 export default App;
