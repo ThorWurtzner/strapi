2556db21916e35fc2ee936c810c5c863e2caa37b
Thor W <6273158@rts-365.dk>
2021-06-06 22:47:52 +0200

    admin page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,76 @@
 import './App.scss';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import axios from "axios";
-import { Router } from "@reach/router";
+import { navigate, Router } from "@reach/router";
 import Home from "./pages/Home/Home";
 import Gallery from "./pages/Gallery/Gallery";
 import Posts from "./pages/Posts/Posts";
 import SimpleReactLightbox from 'simple-react-lightbox';
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Admin from "./pages/Admin/Admin";
+import { useForm } from "react-hook-form";
+import Alert from "@material-ui/lab/Alert";
+
+import TokenContext from "./TokenContext";
 
 
 function App() {
   var [hero, setHero] = useState('');
+  var [open, setOpen] = useState(false);
+  var [token, setToken] = useState('false');
 
   useEffect(() => {
       axios.get("https://strapi-blog-db.herokuapp.com/hero")
         .then(response => setHero(response.data.Hero.url))
-
-      // axios.get("http://localhost:1337/site-color")
-      //   .then(response => setColor(response.data.color))
   }, [setHero])
+
+  var { register, handleSubmit, formState: {errors} } = useForm();
+  var [loginError, setLoginError] = useState('');
+
+  function login(data) {
+    axios.post("http://localhost:1337/auth/local", {
+      "identifier": data.email,
+      "password": data.password
+    })
+      .then(response => {
+        setToken(response.data);
+        navigate("/admin");
+        setLoginError(false);
+        setOpen(false);
+      })
+      .catch(error => setLoginError("Email or password is incorrect"))
+  }
   
   return (
     <div className="app" style={{backgroundImage: `url(${hero})`}}>
+      {/* <TokenContext.Provider value={tokenState}> */}
         <SimpleReactLightbox>
           <Router>
             <Home path="/" />
+            <Admin path="/admin" token={token} />
             <Posts path="/posts" />
             <Posts path="/posts/:id" />
             <Gallery path="gallery" />
           </Router>
         </SimpleReactLightbox>
+      {/* </TokenContext.Provider> */}
+        <div>
+          <button className="loginBtn" onClick={() => setOpen(!open)}><i className="fas fa-user"></i></button>
+
+          <form onSubmit={handleSubmit(login)} noValidate autoComplete="off" className="dropdown" style={{transform: open === true ? "translateX(0)" : "translateX(100%)"}}>
+              <TextField id="standard-basic" label="Username"
+                {...register("email", {required: true})} error={errors.email ? true : false} 
+               />
+
+              <TextField style={{marginTop: "-20px"}} id="standard-basic" label="Password" type="password" 
+                {...register("password", {required: true})} error={errors.password ? true : false} 
+               />
+
+              <Button type="submit" style={{width: "150px"}} variant="contained" color="primary">Log in</Button>
+              {loginError ? <Alert className="login__error" severity="error">{loginError}</Alert> : null}
+          </form>
+        </div>
     </div>
   )
 }
---
+++ src/pages/Admin/Admin.js
@@ -0,0 +1,58 @@
+import { Button, TextField } from '@material-ui/core';
+import { navigate, Link } from '@reach/router'
+import axios from 'axios';
+import React, { useContext } from 'react';
+import TokenContext from '../../TokenContext';
+import Home from '../Home/Home';
+import { useForm } from "react-hook-form";
+import "./Admin.scss";
+
+export default function Admin({token}) {
+
+    var { register, handleSubmit } = useForm();
+
+    if (!token.jwt) {
+        navigate("/");
+        return <Home />
+    }
+
+    function post(data) {
+        axios.post("http://localhost:1337/posts", {
+                Title: data.title,
+                Text: data.text
+            },
+            {
+                headers: {
+                    Authorization: 'Bearer ' + token.jwt
+                }
+            }
+        )
+        .then(response => console.log(response))
+    }
+    
+
+    return (
+        <>
+            <div className="admin">
+            <div className="admin__wrapper">
+                <nav>
+                    <Link to="/">Home</Link>
+                    <Link to="/posts">Indlæg</Link>
+                    <Link to="/gallery">Galleri</Link>
+                </nav>
+                <h2 className="admin__wrapper__name">Velkommen {token.user.username}</h2>
+                <form onSubmit={handleSubmit(post)}>
+                <h2>Opret ny artikel</h2>
+                    <TextField className="admin__input" id="standard-basic" label="Title"
+                        {...register("title", {required: true})}
+                    />
+                    <TextField style={{minHeight: "200px"}} className="admin__input" id="standard-basic" variant="outlined" label="Article" multiline rowsMax="15"
+                        {...register("text", {required: true})}
+                    />
+                    <Button type="submit" color="secondary" variant="contained" style={{width: "150px"}}>Submit post</Button>
+                </form>
+            </div>
+        </div>
+        </>
+    )
+}
